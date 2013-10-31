d00f819ba10925827098da100a50d859597b5e93
Jeff Harwell <jeff.harwell@gmail.com>
2013-10-31 00:26:00 -0700

    Applied specificity bug fix from Chris, fix write bug, added write results to Cassandra, roughed in POS tagger

--- expander.py
+++ expander.py
@@ -43,6 +43,55 @@ class GoogleExpansionFailure(Exception):
         fails.
     """
     pass
+class UnknownExpansionType(Exception):
+    """ A custom exception class thrown when you try to write an expansion to Cassandra
+        that is not a corpus or google search type
+    """
+    pass
+
+class PartOfSpeech():
+    """Returns a Part of Speach for a given term"""
+    def __init__(self):
+        self.possessive = re.compile(r"'s")
+        self.punctuation = re.compile(r"^[\.\^\*\+\?\{\}\[\]\(\)\\\|\-,;:%@!&/_]+$")
+        self.numeric = re.compile(r"\d+")
+
+        ## http://grammar.yourdictionary.com/parts-of-speech/conjunctions/conjunctions.html
+        c1 = ['and','but','or','nor','for','yet','so','after','although','as','because','before','even','if','inasmuch','in']
+        c2 = ['just','lest','now','once','provided','rather','since','so','supposing','than','that','though']
+        c3 = ['til','unless','until','when','whenever','where','whereas','where if','wherever','whether']
+        c4 = ['which','while','who','whoever','why']
+        c5 = ['both','either','neither','scarcely','rather']
+        self.conjunction = c1 + c2 + c3 + c4 + c5
+        
+        ## http://en.wikipedia.org/wiki/List_of_English_prepositions
+        p1 = ['a','abaft','aboard','about','above','absent','across','afore','after','against','along']
+        p2 = ['alongside','amid','amidst','among','amongst','an','anenst','apropos','apud','around']
+        p3 = ['as','aside','astride','at','athwart','atop','barring','before']
+        p4 = ['behind','below','beneath','beside','besides','between','beyond','but','by']
+        p5 = ['circa','concerning','despite','down','during','except','excluding','failing']
+        p6 = ['following','for','forenenst','from','given','in','including','inside','into','lest','like']
+        p7 = ['mid','midst','minus','modulo','near','nigh','next','notwithstanding','of','off','on']
+        p8 = ['onto','opposite','out','outside','over','pace','past','per','plus','pro','qua']
+        p9 = ['regarding','round','sans','save','since','than','through','thru','throughout']
+        p10 = ['till','times','to','toward','towards','under','underneath','unlike','until','unto']
+        p11 = ['up','upon','versus','via','vice','with','within','without','worth']
+        self.preposition = p1+p2+p3+p4+p5+p6+p7+p8+p9+p10+p11
+    def getPOS(self,term):
+        if (self.possessive.match(term)):
+            return "possessive"
+        elif (self.punctuation.match(term)):
+            return "punctuation"
+        elif (self.numeric.match(term)):
+            return "numeric"
+        elif (term in self.preposition):
+            return "preposition"
+        elif (term in self.conjunction):
+            return "conjunction"
+        else:
+            return ""
+        
+        
 
 class Expander():
     """Given a file that contains queries on separate lines,
@@ -60,6 +109,7 @@ class Expander():
         reader=csv.reader(opened)
         self.terms=[]
         self.total_count=0
+        self.pos = PartOfSpeech()
         for term in reader:
             self.terms.append(term[0])
         
@@ -112,15 +162,49 @@ class Expander():
 
         return self.insert_data(new_terms,term,"search")
 
+    def write_results_to_cassandra(self,dic):
+        for word_key in dic.keys():
+            terms = dic[word_key]['term'].split()
+            etype = dic[word_key]['type']
+            ambiguity = dic[word_key]['amb']
+            specificity = dic[word_key]['spe']
+            row=[word_key,dic[word_key]['type'],dic[word_key]['amb'],dic[word_key]['spe'],dic[word_key]['term']]
+            pos = self.pos.getPOS(word_key)
+            
+            table_name = None
+            if (etype == 's'):
+                table_name = 'fourgram_search_expansion'
+            elif (etype == 'c'):
+                table_name = 'fourgram_corpus_expansion'
+            else:
+                print "Unknown exansion type %s for term %s" % (term, etype)
+                raise UnknownExpansionType()
+            query = """
+	    insert into fourgm.%s (gramone, gramtwo, gramthree, expansion, specificity, ambiguity, pos)
+	    values (:gramone, :gramtwo, :gramthree, :expansion, :specificity, :ambiguity, :pos)
+            """ % (table_name)
+            values = {"gramone":terms[0], "gramtwo":terms[1], "gramthree":terms[2], "expansion":word_key, "specificity":specificity, "ambiguity":ambiguity, "pos":pos}
+            for i in range(20):
+                ## The Database might time out, if so back off and try again
+                try:
+                    self.cursor.execute(query, values)
+                except cql.apivalues.OperationalError:
+                    print "Timeout, backoff and retry"
+                    time.sleep(20)
+                    continue
+                else:
+                    break
+            
+
     def top_index_expand(self,term,count):
 	## Use the cassandra database to get the Google Corpus terms
 	## as opposed to the SQL server database
-	self.top_index_expand_cassandra(term,count)
+	return self.top_index_expand_cassandra(term,count)
 
     def top_index_expand_cassandra(self,term,count):
         """ Method which expands the query using the Google Web Corpus parsed and stored
             in a Cassandra database
-        """"
+        """
         print "Corpus Expanding %s" % term
         terms = term.split()
         ## We are only set up to do expansions on 3-gram searches ... so check for this
@@ -267,14 +351,14 @@ class Expander():
         self.connection is for NGram Corpus on Cassandra
         self.connection2 is for UMLS on SQLServer 2008
         """
-        ##self.connection = pyodbc.connect('DRIVER={SQL Server};Trusted_Connection=yes;SERVER=WIN-20IR78KE8UV\SQLSERVER2012;DATABASE=GoogleCorpus;UID=XXXXXXXX')
+        ##self.connection = pyodbc.connect('DRIVER={SQL Server};Trusted_Connection=yes;SERVER=WIN-20IR78KE8UV\SQLSERVER2012;DATABASE=GoogleCorpus;UID=XXXXXXXXX')
         ##self.cursor = self.connection.cursor()
         ## This is the cassandra database with the 4-gram Google Web Corpus
         
         self.connection = cql.connect('127.0.0.1', 9160, "fourgm", cql_version = '3.0.0')
         self.cursor = self.connection.cursor()
 
-        self.connection2 = pyodbc.connect('DRIVER={SQL Server};SERVER=127.0.0.1;DATABASE=umlsSmall;UID=XXXXXXXX;PWD=XXXXXXXX')
+        self.connection2 = pyodbc.connect('DRIVER={SQL Server};SERVER=134.173.236.21;DATABASE=umlsSmall;UID=XXXXXXX;PWD=XXXXXX')
         self.cursor2=self.connection2.cursor()
 
     def write_to_file(self,filename,data):
@@ -330,10 +414,14 @@ def main():
         c+=1
         print "%d / %d" % (c,l)
         try:
+            print "Top Index Expand"
             d=expander.top_index_expand(term.lower(),10)
+            print "Got %s" % d
             if d:
                 for dic in d:
+                    print "Processing Result %s" % dic
                     data.append(dic)
+                    expander.write_results_to_cassandra(dic)
         except NotThreeGram:
             ## expander will throw a NotThreeGram exception if it is asked to
             ## parse a search that does not contain exactly three grams.
